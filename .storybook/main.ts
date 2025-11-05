1690c5e6adc16536340852a152aff7ded5812076
Nari Jeong <92130993+naringst@users.noreply.github.com>
2025-11-06 01:56:41 +0900

    chore: storybook 각 프로퍼티 기능 주석 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,22 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
+/**
+ * storybook에 가장 중요한 설정 파일
+ */
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  // 편의 기능을 제공하는 애드온들
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  // storybook 구동 시 필요한 framework 설정
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
 };
 export default config;
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,13 +3,21 @@ import React from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 
-// MUI 테마 설정
+/**
+ * story 렌더링에 필요한 공통 전역 설정
+ */
+
 const theme = createTheme();
 
 const preview: Preview = {
+  // metadata를 정의해서 story에 다양한 기능을 추가할 때 사용
+  // actions: event handler 실행 시 받은 데이터를 story에 표시하는 역할
   parameters: {
+    // controls: 개발자가 따로 코드를 변경하지 않고도 storybook에 arguments를 동적으로 바꿔가며 인터렉션 할 수 있게 하는 기능
+    // Storybook UI에서 하단 controls 바꾸면 story에 반영되어 바뀌어서 렌더링 됨.
     controls: {
       matchers: {
+        // 색이나 날짜는 color picker, date picker로 가능하도록 preview에 설정된 것.
         color: /(background|color)$/i,
         date: /Date$/i,
       },
@@ -25,8 +33,18 @@ const preview: Preview = {
       viewports: [768, 1024, 1280],
       delay: 500,
     },
+    // msw: {
+    //   handlers,
+    // },
   },
+  // 주로 스토리 렌더링 할 때 특정 컴포넌트로 감싸거나 sibiling 컴포넌트를 추가하는 설정 할 때 사용
+  // 특정 context를 항상 감싸서 스토리를 렌더링 해야 할 때 유용
+  // react hook form , react router 사용하면 with Router, withRHF 사용 가능
+  // msw에 대한 설정도 storybook에서 하면 좋음 storybook에서는 굳이 실제 api를 스토리를 렌더링할 필요 없음
+  // 즉, msw로 api mocking해서 storybook에서 사용하면 좋음
+  // 각 컴포넌트 스토리에서 msw 요청을 보낼 때
   decorators: [
+    // mswDecorator,
     (Story) => (
       <ThemeProvider theme={theme}>
         <CssBaseline />
